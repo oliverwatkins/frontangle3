706bbea4a4bff889f18317c6b114f19aafb8965e
oliverwatkins <oliver.f.watkins@gmail.com>
2023-02-04 17:23:36 +0100

    contact form

--- src/js/pages/ContactPage.js
+++ src/js/pages/ContactPage.js
@@ -3,6 +3,8 @@ import React from "react";
 import "./contactPage.scss"
 import { useTranslation } from 'react-i18next';
 
+import linkedInLogo from './../../img/link-linkedin.png';
+import xingLogo from './../../img/xing.svg';
 
 
 export default function ContactPage() {
@@ -11,38 +13,77 @@ export default function ContactPage() {
     // return <h1>{t('Welcome to React')}</h1>
     // i18n.changeLanguage("en")
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Currently the contact form is broken and I am working on fixing it. In the meantime please email me at oliver.f.watkins@gmail.com")
+    }
 
     return (
-
         <section className="fa-page contact-page">
-
-            <div className="mugshot-image"/>
-
-            {t('Welcome to React')}
-
+            {/*<div className="mugshot-image"/>*/}
 
             <div className="contact-info">
-                <h1>Contact </h1>
-                On social media :
-                <ul>
-                    <li>
-                        <a href="https://www.linkedin.com/in/oliver-watkins-0673b27/"> Linkedin </a>
-                    </li>
-                    <li>
-                        <a href="https://www.xing.com/profile/Oliver_Watkins2"> Xing </a>
-                    </li>
-                </ul>
-                <p/>
-                Email : <b>oliver.f.watkins[at]gmail.com</b>
+                {/*<h1>Contact </h1>*/}
+
+
+                <p>
+
+                If you would like to contact me please fill out the form on the right.
+                You can also contact me via <a href={"https://www.linkedin.com/in/oliver-watkins-0673b27/"}>Linked In </a>
+                    or <a href={"https://www.xing.com/profile/Oliver_Watkins2/cv"}> Xing</a>
+                </p>
+
+                <div className="contact-page-image">  </div>
+
+                {/*On social media :*/}
+
+
+                {/*<ul>*/}
+                {/*    <li>*/}
+                {/*        <a href="https://www.linkedin.com/in/oliver-watkins-0673b27/"> Linkedin </a>*/}
+                {/*    </li>*/}
+                {/*    <li>*/}
+                {/*        <a href="https://www.xing.com/profile/Oliver_Watkins2"> Xing </a>*/}
+                {/*    </li>*/}
+                {/*</ul>*/}
+                {/*<p/>*/}
+                {/*Email : <b>oliver.f.watkins[at]gmail.com</b>*/}
             </div>
+            <div className="contact-form-container">
+                <form
+                    // action="https://api.web3forms.com/submit"
+                    onSubmit={handleSubmit}
+                >
+                    {/*16efebb6-ce76-4ef8-94c8-6b97563e5d3f*/}
+                    <input type="hidden" name="access_key" value="16efebb6-ce76-4ef8-94c8-6b97563e5d3f" />
 
-            <div className="contact-page-image"/>
+                    <label htmlFor="name">First Name</label>
 
+                    <input type="text" name="name" required placeholder="Your name.." />
 
+                    {/*<input type="text" id="fname" name="firstname" placeholder="Your name.." />*/}
 
+                    <label htmlFor="lname">Last Name</label>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
+                    <label htmlFor="country">Country</label>
+                    <select id="country" name="country">
+                        <option value="australia">Australia</option>
+                        <option value="canada">Canada</option>
+                        <option value="usa">USA</option>
+                    </select>
+
+                    <label htmlFor="subject">Subject</label>
+                    <textarea id="subject" name="subject" placeholder="Write something.." style={{height:"200px"}}/>
+                    <input type="hidden" name="redirect" value="https://web3forms.com/success" />
+                    <input type="submit" value="Submit" />
+
+                    <div className={"contact-page-links"}>
+                        <a href={"https://www.linkedin.com/in/oliver-watkins-0673b27/"}><img src={xingLogo} alt="Linked in"/></a>
+                        <a href={"https://www.xing.com/profile/Oliver_Watkins2/cv"}><img src={linkedInLogo} alt="Xing"/></a>
+                    </div>
+                </form>
+            </div>
         </section>
-
-
     );
 }
